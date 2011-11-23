fa9eea96740b9b48644c322aae2db7a01dcd8f62
Davies Liu <davies.liu@gmail.com>
2011-11-23 17:17:54 +0800

    add daily_weblog.py, bugfix of hourly_weblog.py: not ignore lines

---
+++ examples/daily_weblog.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import sys, os, os.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from datetime import date,timedelta
+from operator import itemgetter, add
+import subprocess
+from dpark import DparkContext
+import pickle
+dpark = DparkContext()
+import gc
+gc.disable()
+
+sinkservers = ['balin', 'theoden']
+log_path = "/mfs/log/access-log/current/weblog/%s/%s"
+DATE,TIME,UID,IP,BID,METHOD,NURL,URL,CODE,LENGTH,PT,NREFERER,REFERER = range(13)
+LIMITS = [10,8,11,12,20,8,80,255,3,8,5,80,255] 
+
+def drop_args(url):
+    return url[:url.index('?')] if '?' in url else url
+
+def clean(topr):
+    topr = dpark.broadcast(topr)
+    def _(line):
+        line = [v.replace(',','%2C')[:LIMITS[i]] for i,v in enumerate(line)]
+        line[NURL] = drop_args(line[NURL]) 
+        line[NREFERER] = drop_args(line[NREFERER]) 
+        if line[NREFERER] not in topr.value:
+            line[NREFERER] = ''
+        return (line[NURL], ','.join(line))
+    return _
+
+def drop_nurl(l):
+    l = l.split(',')
+    l[NURL] = ''
+    return ','.join(l)
+
+def load_weblog(day):
+    path = '/mfs/tmp/daily_weblog/%s' % day.strftime("%Y%m%d")
+    if not os.path.exists(path):
+        weblog = dpark.csvFile('/mfs/log/weblog/%s' % day.strftime("%Y/%m/%d"), splitSize=16<<20)
+        topreferers = weblog.map(lambda l:(drop_args(l[NREFERER]), 1)).reduceByKey(add).filter(lambda (x,y): y>1000).collectAsMap()
+        g = weblog.map(clean(topreferers)).groupByKey()
+        s = g.flatMap(
+                lambda (u,ls): len(ls) > 1000 and ls or [drop_nurl(l) for l in ls]
+            ).saveAsTextFile(path, ext='csv')
+    
+    # ib
+    for name in os.listdir(path):
+        if name.startswith('.'):
+            continue
+#        if not name.endswith("csv"):
+#            continue
+        flag = os.path.join(path, name+".done")
+        if os.path.exists(flag):
+            continue
+        cmd = "mysql -hbalin -uluzong -pfulllink -P4406 rivendell -e".split(' ')
+        cmd.append(r"LOAD DATA INFILE '%s' INTO TABLE weblog FIELDS TERMINATED by ',' ENCLOSED BY '\"'"
+                  % os.path.join(path,name))
+        print ' '.join(cmd)
+        p = subprocess.Popen(cmd)
+        p.wait()
+        if p.returncode == 0:
+            open(flag, 'w').write('OK')
+        else:
+            print 'load failed', os.path.join(path,name)
+
+if __name__ == '__main__':
+    today = date.today() 
+    for i in range(1, 2):
+        day = today - timedelta(days=i)
+        print day
+        load_weblog(day)
--- examples/hourly_weblog.py
+++ examples/hourly_weblog.py
@@ -3,6 +3,8 @@ import sys, os, os.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from datetime import datetime,timedelta
 from operator import itemgetter
+import csv
+from cStringIO import StringIO
 import subprocess
 from dpark import DparkContext
 import pickle
@@ -11,12 +13,25 @@ dpark = DparkContext()
 sinkservers = ['balin', 'theoden']
 log_path = "/mfs/log/access-log/current/weblog/%s/%s"
 DATE,TIME,UID,IP,BID,METHOD,NURL,URL,CODE,LENGTH,PT,NREFERER,REFERER = range(13)
+LIMITS = [10,8,11,12,20,8,255,255,3,8,5,255,255]
 
 def format_weblog(hour):
     logs = [dpark.textFile(log_path % (h,hour.strftime("%Y/%m/%d/%H/")), splitSize=10<<20)
              for h in sinkservers]
     rawlog = dpark.union(logs)
     return rawlog.pipe('/mfs/log/nginx-log/format_access_log --stream')
+
+def parse(line):
+    return csv.reader(StringIO(line)).next()
+
+def drop_args(url):
+    return url[:url.index('?')] if '?' in url else url
+
+def clean(line):
+    line = [v.replace(',','%2C')[:LIMITS[i]] for i,v in enumerate(line)]
+    line[NURL] = drop_args(line[NURL])
+    line[NREFERER] = drop_args(line[NREFERER])
+    return (line[NURL], ','.join(line))
 
 def drop_nurl(line):
     ps = line.split(',')
@@ -27,22 +42,13 @@ def load_weblog(hour):
     path = '/mfs/tmp/hourly_weblog/%s' % hour.strftime("%Y%m%d%H")
     if not os.path.exists(path):
         weblog = format_weblog(hour)
-        LIMITS = [10,8,11,12,20,8,255,255,3,8,5,255,255] 
-        g = weblog.map(lambda line:line.split(",")).filter(
-                lambda line:len(line) == 13
-            ).map(
-                lambda l:(l[NURL], ",".join(v[:LIMITS[i]] for i,v in enumerate(l)))
-            ).groupByKey()
-        #    cnt = g.mapValue(len).collectAsMap()
-        #    topnurl = sorted(cnt.iteritems(), key=itemgetter(1), reverse=True)[:5000]
-        #    print len(cnt), topnurl[-10:]
-        #    topnurl = dict(topnurl)
+        g = weblog.map(parse).map(clean).groupByKey()
         s = g.flatMap(
                 lambda (u,ls): len(ls) > 10 and ls or [drop_nurl(l) for l in ls]
             ).saveAsTextFile(path, ext='csv')
-
-    # ib
+    
     for name in os.listdir(path):
+        if name.startswith('.'): continue
         if not name.endswith("csv"):
             continue
         flag = os.path.join(path, name+".done")
@@ -61,6 +67,6 @@ def load_weblog(hour):
 
 if __name__ == '__main__':
     now = datetime.now() 
-    for i in range(1, 10):
+    for i in range(1, 24):
         hour = now - timedelta(seconds=3600*i)
         load_weblog(hour)
