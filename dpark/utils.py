016bd6d7e0838bb58805d8c68636db1c579a76d2
Davies Liu <davies.liu@gmail.com>
2012-09-12 17:38:32 +0800

    use lz4 as compress

--- dpark/utils.py
+++ dpark/utils.py
@@ -3,13 +3,14 @@ from zlib import compress as _compress, decompress
 
 def compress(s):
     return _compress(s, 1)
-#try:
-#    from snappy import compress, decompress
-#except ImportError:    
-#    try:
-#        from snappy import compress, decompress
-#    except ImportError:
-#        pass
+try:
+    from lz4 import compress, decompress
+except ImportError:    
+    print 'no lz4 available'
+    #try:
+    #    from snappy import compress, decompress
+    #except ImportError:
+    #    pass
 
 try:
     from setproctitle import getproctitle, setproctitle
