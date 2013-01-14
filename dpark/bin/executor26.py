c032fb2c6c9c1af011d7f22c4aa3d2a9a03cb7cd
Davies Liu <davies.liu@gmail.com>
2013-01-14 16:41:59 +0800

    fix sys.path

--- dpark/bin/executor26.py
+++ dpark/bin/executor26.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.6
 import sys
 from os.path import dirname
-sys.path.insert(1, dirname(dirname(dirname(__file__))))
+sys.path.insert(1, dirname(dirname(__file__)))
 
-from dpark.executor import run
+from executor import run
 
 if __name__ == '__main__':
     run()
