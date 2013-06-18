5b2fddaaffb3fe601f0865ab1070b8e5071fc931
Davies Liu <davies.liu@gmail.com>
2013-06-18 12:15:42 +0800

    check outputLocs

--- dpark/schedule.py
+++ dpark/schedule.py
@@ -312,7 +312,7 @@ class DAGScheduler(Scheduler):
                 elif isinstance(task, ShuffleMapTask):
                     stage = self.idToStage[task.stageId]
                     stage.addOutputLoc(task.partition, evt.result)
-                    if not pendingTasks[stage]:
+                    if not pendingTasks[stage] and all(stage.outputLocs):
                         logger.debug("%s finished; looking for newly runnable stages", stage)
                         running.remove(stage)
                         if stage.shuffleDep != None:
@@ -328,12 +328,10 @@ class DAGScheduler(Scheduler):
                             submitMissingTasks(stage)
             elif isinstance(reason, FetchFailed):
                 if stage in running:
-                    running.remove(stage)
                     waiting.add(stage)
                 mapStage = self.shuffleToMapStage[reason.shuffleId]
                 mapStage.removeHost(reason.serverUri)
                 failed.add(mapStage)
-                del pendingTasks[stage]
                 lastFetchFailureTime = time.time()
             else:
                 logger.error("task %s failed: %s %s %s", task, reason, type(reason), reason.message)
