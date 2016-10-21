9811b81a7e88c1c2800ff03b232958de9fd7149e
lianwenbo <lianwenbo@douban.com>
2016-10-21 18:39:23 +0800

    bump version to 0.3.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if 'setuptools.extension' in sys.modules:
 
 
 ext_modules = [Extension('dpark.portable_hash', ['dpark/portable_hash.pyx'])]
-version = '0.3.4'
+version = '0.3.5'
 
 setup(name='DPark',
       version=version,
