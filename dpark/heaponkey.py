868d25f2671844e8fe4442024a74e678f37cb053
yangxiufeng <yangxiufeng@douban.com>
2017-12-07 16:21:42 +0800

    Add heapOnKey.merge to merge sorted iterators

--- dpark/heaponkey.py
+++ dpark/heaponkey.py
@@ -4,7 +4,17 @@ from dpark.serialize import load_func, dump_func
 import sys
 from six.moves import range
 
-class HeapOnKey:
+
+if sys.version_info[0] < 3:
+    def next_func(it):
+        return it.next
+else:
+    def next_func(it):
+        return it.__next__
+
+
+class HeapOnKey(object):
+
     def __init__(self, key=None, min_heap=False):
         self.key = key
         self.min_heap = min_heap
@@ -73,8 +83,57 @@ class HeapOnKey:
             pos = child_pos
             child_pos = 2 * pos + 1
 
+    def replace(self, heap, item):
+        returnitem = heap[0]    # raises appropriate IndexError if heap is empty
+        heap[0] = item
+        self._sift_up(heap, 0)
+        return returnitem
 
-if __name__ == '__main__':
+    def merge(self, iterables, ordered_iters=0):
+        """iterables: each sorted with the same key
+            ordered_iters:
+                when come to equal value, the element in the first iter yields
+                    first(last) if ordered_iters >(<) 0
+                    not stable if ordered_iters == 0
+        """
+        if not ordered_iters:
+            key = lambda x: self.key(x[0])
+        else:
+            key = lambda x: (self.key(x[0]), x[1])
+        heap = HeapOnKey(key, self.min_heap)
+        _heappop, _heapreplace, _StopIteration = heap.pop, heap.replace, StopIteration
+        _len = len
+
+        h = []
+        h_append = h.append
+        order = -1 if ordered_iters and ((ordered_iters > 0) ^ self.min_heap) else 1
+
+        for it_idx, it in enumerate(map(iter, iterables)):
+            try:
+                _next = next_func(it)
+                h_append([_next(), order * it_idx, _next])
+            except _StopIteration:
+                pass
+        heap.heapify(h)
+
+        while _len(h) > 1:
+            try:
+                while 1:
+                    v, _, _next = s = h[0]
+                    yield v
+                    s[0] = _next()               # raises StopIteration when exhausted
+                    _heapreplace(h, s)          # restore heap condition
+            except _StopIteration:
+                _heappop(h)                     # remove empty iterator
+        if h:
+            # fast case when only a single iterator remains
+            v, _, _next = h[0]
+            yield v
+            for v in _next.__self__:
+                yield v
+
+
+def test():
     l = [10, 9, 20, 18, 3, 24, 29, 39]
     h = HeapOnKey()
     h.heapify(l)
@@ -153,3 +212,7 @@ if __name__ == '__main__':
             top_l.pop()
 
     print('after sort:', top_l, ' with elapsed:', time.time() - start, ' with cnt:', call_cnt / 2, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    test()
---
+++ tests/test_heaponkey.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import random
+import sys
+import os
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from dpark.heaponkey import HeapOnKey
+from pprint import pprint
+
+
+class TestHeap(unittest.TestCase):
+
+    def test_merge(self):
+        N = 100
+        n = 13
+        a = list(range(N))
+        random.shuffle(a)
+        a = list(enumerate(a))
+        b = a
+        lsts = []
+        while len(b):
+            lsts.append(b[:n])
+            b = b[n:]
+
+        key = lambda x: x[1]
+        lsts = list(map(lambda x: sorted(x, key=key), lsts))
+        # pprint(lsts)
+
+        h = HeapOnKey(key=key, min_heap=True)
+        r = list(h.merge(lsts))
+        exp = sorted(a, key=key)
+        # pprint(exp)
+        # pprint(r)
+
+        assert r == exp
+
+
+if __name__ == "__main__":
+    unittest.main()
