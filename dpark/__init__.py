27b158fc411a2634789aedac444bd2198a603976
tianzhongbo <tianzhongbo@douban.com>
2018-01-04 16:20:26 +0800

    remove deprecated module-level api

--- dpark/__init__.py
+++ dpark/__init__.py
@@ -1,32 +1,5 @@
 from __future__ import absolute_import
 from .context import DparkContext, parser as optParser
 from .bagel import Bagel
-from .decorator import jit
 
-_ctx = DparkContext()
-
-parallelize = _ctx.parallelize
-
-makeRDD = _ctx.makeRDD
-
-textFile = _ctx.textFile
-
-partialTextFile = _ctx.partialTextFile
-
-csvFile = _ctx.csvFile
-
-binaryFile = _ctx.binaryFile
-
-tableFile = _ctx.tableFile
-
-beansdb = _ctx.beansdb
-
-union = _ctx.union
-
-zip = _ctx.zip
-
-start = _ctx.start
-
-stop = _ctx.stop
-
-clear = _ctx.clear
+__all__ = ['DparkContext', 'optParser', 'Bagel']
