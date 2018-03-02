e59ecc2946b7d86eaad97cf17fb18a21b347a41a
tianzhongbo <tianzhongbo@douban.com>
2018-03-02 15:55:39 +0800

    fix include order

--- dpark/crc32c_mod.c
+++ dpark/crc32c_mod.c
@@ -1,5 +1,5 @@
-#include <stdint.h>
 #include <Python.h>
+#include <stdint.h>
 
 uint32_t crc32c(const void *buf, size_t len, uint32_t crc);
 uint32_t crc32c_sw(uint32_t crci, const void *buf, size_t len);
