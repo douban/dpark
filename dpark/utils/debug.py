875b70d22a2a2e5ee6bd6b21e4b9c3b4fb150127
yangxiufeng <yangxiufeng@douban.com>
2018-07-18 20:00:24 +0800

    Refactor import.

--- dpark/utils/debug.py
+++ dpark/utils/debug.py
@@ -5,11 +5,9 @@ def spawn_rconsole(env):
     rfoo_logger = logging.getLogger('rfoo')
     rfoo_logger.disabled = 1
     try:
-        import rfoo.utils.rconsole
-        import rfoo._rfoo
-        rfoo._rfoo.logging = rfoo.utils.rconsole.logging = rfoo_logger
-
         from rfoo.utils import rconsole
+        import rfoo._rfoo
+        rfoo._rfoo.logging = rconsole.logging = rfoo_logger
         rconsole.spawn_server(env, 0)
     except ImportError:
         pass
